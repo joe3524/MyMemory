8b2cc35356525fcae654e75cceab3e1f105049a2
Joe K <joe3524@gmail.com>
2017-11-17 16:29:10 +0900

    updating the profile area on the side bar (p.814 ~ 817)

--- MyMemory/SideBarVC.swift
+++ MyMemory/SideBarVC.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class SideBarVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let uinfo = UserInfoManager()   // 개인 정보 관리 매니저
     
     // 목록 데이터 배열
     let titles = ["새글 작성하기", "친구 새글", "달력으로 보기", "공지사항", "통계", "계정 관리"]
@@ -40,7 +41,7 @@ class SideBarVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         
         // 이름 레이블의 속성을 정의하고, 헤더 뷰에 추가한다.
         self.nameLabel.frame = CGRect(x: 70, y: 15, width: 100, height: 30)
-        self.nameLabel.text = "꼼꼼한 재은 씨"    // 기본 텍스트
+//        self.nameLabel.text = "꼼꼼한 재은 씨"    // 기본 텍스트
         self.nameLabel.textColor = UIColor.white    // 텍스트 색상
         self.nameLabel.font = UIFont.boldSystemFont(ofSize: 15)
         self.nameLabel.backgroundColor = UIColor.clear
@@ -49,7 +50,7 @@ class SideBarVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         
         // 이메일 레이블의 속성을 정의하고, 헤더 뷰에 추가한다.
         self.emailLabel.frame = CGRect(x: 70, y: 30, width: 100, height: 30)
-        self.emailLabel.text = "sqlpro@naver.com"   // 기본 텍스트
+//        self.emailLabel.text = "sqlpro@naver.com"   // 기본 텍스트
         self.emailLabel.textColor = UIColor.white   // 텍스트 색상
         self.emailLabel.font = UIFont.systemFont(ofSize: 11)    // 폰트 사이즈
         self.emailLabel.backgroundColor = UIColor.clear     // 배경 색상
@@ -57,8 +58,8 @@ class SideBarVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         headerView.addSubview(self.emailLabel)  // 헤더 뷰에 추가
         
         // 기본 이미지를 구현한다.
-        let defaultProfile = UIImage(named: "account.jpg")
-        self.profileImage.image = defaultProfile    // 이미지 등록
+//        let defaultProfile = UIImage(named: "account.jpg")
+//        self.profileImage.image = defaultProfile    // 이미지 등록
         self.profileImage.frame = CGRect(x: 10, y: 30, width: 50, height: 50)
         
         // 추가) 프로필 이미지 둥글게 만들기
@@ -67,6 +68,14 @@ class SideBarVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         self.profileImage.layer.masksToBounds = true    // 마스크 효과
         
         view.addSubview(self.profileImage)  // 헤더 뷰에 추가
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.nameLabel.text = self.uinfo.name ?? "Guest"
+        self.emailLabel.text = self.uinfo.account ?? ""
+        self.profileImage.image = self.uinfo.profile
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
